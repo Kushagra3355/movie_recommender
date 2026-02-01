3ddfd365880484bf8fc8afb37fca8d32766077e1
Kushagra3355 <kushagra.omar@gmail.com>
2026-02-01 15:57:59 +0530

    Add poster fetching feature and use Git LFS for large files

--- app.py
+++ app.py
@@ -7,16 +7,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# def fetch_poster(movie_id):
-#     api_key = os.getenv("TMDB_API_KEY")
-#     url = "https://api.themoviedb.org/3/movie/{}?api_key={}&language=en-US".format(
-#         movie_id, api_key
-#     )
-#     data = requests.get(url)
-#     data = data.json()
-#     poster_path = data["poster_path"]
-#     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-#     return full_path
+def fetch_poster(movie_id):
+    api_key = os.getenv("TMDB_API_KEY")
+    if not api_key:
+        return None
+    try:
+        url = "https://api.themoviedb.org/3/movie/{}?api_key={}&language=en-US".format(
+            movie_id, api_key
+        )
+        resp = requests.get(url, timeout=5)
+        resp.raise_for_status()
+        data = resp.json()
+        poster_path = data.get("poster_path")
+        if not poster_path:
+            return None
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+        return full_path
+    except Exception:
+        return None
 
 
 def recommend(movie):
@@ -25,14 +33,18 @@ def recommend(movie):
         list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1]
     )
     recommended_movie_names = []
-    # recommended_movie_posters = []
+    recommended_movie_posters = []
     for i in distances[1:6]:
         # fetch the movie poster
-        # movie_id = movies.iloc[i[0]].movie_id
-        # recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        movie_row = movies.iloc[i[0]]
+        movie_id = movie_row.get("movie_id") or movie_row.get("id")
+        poster_url = None
+        if movie_id is not None:
+            poster_url = fetch_poster(movie_id)
+        recommended_movie_posters.append(poster_url)
+        recommended_movie_names.append(movie_row.title)
 
-    return recommended_movie_names  # , recommended_movie_posters
+    return recommended_movie_names, recommended_movie_posters
 
 
 st.header("Movie Recommender System")
@@ -43,22 +55,14 @@ movie_list = movies["title"].values
 selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
 
 if st.button("Show Recommendation"):
-    recommended_movie_names = recommend(selected_movie)
-    # recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        # st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        # st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        # st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        # st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        # st.image(recommended_movie_posters[4])
+    cols = [col1, col2, col3, col4, col5]
+    for idx, col in enumerate(cols):
+        with col:
+            st.text(recommended_movie_names[idx])
+            poster = recommended_movie_posters[idx]
+            if poster:
+                st.image(poster)
+            else:
+                st.write("No poster available")
